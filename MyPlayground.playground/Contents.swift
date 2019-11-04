7d1d44266f9dfd7801e2d9b679580cbd5139a8c0
cy <2248895786@qq.com>
2019-11-04 23:33:46 +0800

    1.创建自己的Playground 2.上传官方Playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// Playground：
+// 不需要项目
+// 直接写代码，立刻看结果
+// 学习语音，测试代码
+
+var str = "Hello, playground"
+
+// OC [[UIView alloc] initWithFrame:CGRectMake...]
+// initWithXXX -> (XXX)
+let v = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+
+// OC [UIColor redColr] 类方法 -> 直接点
+v.backgroundColor = UIColor.red
+
+// OC
+let btn = UIButton(type: .contactAdd)
+btn.center = v.center
+v.addSubview(btn)
